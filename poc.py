44496208fef69a96b17e602ea8bafefbcb2730f3
Gowthami Kummari <gk22204@corp.ojas-it.com>
2022-08-24 14:48:26 +0530

    mesg operators

---
+++ poc.py
@@ -0,0 +1,42 @@
+##poc calculator.py
+
+
+def som(n1,n2):
+    return n1+n2
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))      
+print(som(n1,n2))
+
+          
+def difference(n1,n2):
+    return n1-n2
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))  
+print(difference(n1,n2))
+
+def product(n1,n2):
+    return n1* n2
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))  
+print(product(n1,n2))
+
+
+def division(n1,n2):
+    return n1/n2
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))  
+print(division(n1,n2))
+
+
+def  modulo(n1,n2):
+    return(n1%n2)
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))  
+print(modulo(n1,n2))
+
+def floordivision(n1,n2):
+    return(n1//n2)
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))  
+print(floordivision(n1,n2))
+
